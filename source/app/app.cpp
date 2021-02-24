a73879133ba4689ea38d47c177fb10187fc076c4
Linux1 <ailyin@ucsd.edu>
2021-02-23 21:11:27 -0800

    fix app.cpp

--- source/app/app.cpp
+++ source/app/app.cpp
@@ -7,7 +7,6 @@ using namespace std;
 
 
 int main(){
-    cout << cpp.getTracks("0psS4i5YooJrXfDnGvWRLi")[1] << endl;
     //cout << ac.auth() << endl;
     //cout << cpp.search("weezer", "track", std::map<std::string, std::string>{}, 2, 0)[1] << endl;
 
